4b9262f74ca0d75179d37cba8567f38b433ef96a
A.Harshitha <119464978+Harshitha-reddy88@users.noreply.github.com>
2023-04-20 15:51:03 +0530

    integer to string

---
+++ week_01/day_1/intstring.java
@@ -0,0 +1,15 @@
+public class Main
+{
+ public static void main(String[] args)
+ {
+    int a=8;
+    String b=""+a;
+    System.out.println(b);
+    
+  // to check the type of the variable y.
+   
+    System.out.println(((Object)b).getClass().getSimpleName());
+  }
+ }
+    
+   
