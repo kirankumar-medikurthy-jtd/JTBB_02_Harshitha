32c5e320c0c653103245ea17b34345ee9fa0b063
A.Harshitha <119464978+Harshitha-reddy88@users.noreply.github.com>
2023-04-20 15:52:35 +0530

    reverse of a string

---
+++ week_01/day_1/reverse1.java
@@ -0,0 +1,13 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+    String str="harshitha";
+    String y="";
+    for(int i=0;i<str.length();i++)
+    {
+      y=str.charAt(i)+y;
+    }
+     System.out.println(y);
+   }
+}
