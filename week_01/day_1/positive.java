80f442a8ef30bf207c7072e4fde83f9e6af3eaef
A.Harshitha <119464978+Harshitha-reddy88@users.noreply.github.com>
2023-04-20 14:47:47 +0530

    Add files via upload
    given num is positive or not

---
+++ week_01/day_1/positive.java
@@ -0,0 +1,21 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+   int num=8;
+   System.out.println(positiveOrNegative(num));
+   
+ }
+ public static boolean positiveOrNegative(int num)
+ {
+   if(num>=0)
+   {
+     return true;
+   }
+   else
+   {
+     return false;
+   }
+ }
+  
+}     
