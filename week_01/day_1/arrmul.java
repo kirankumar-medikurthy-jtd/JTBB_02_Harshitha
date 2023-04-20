5036592297616f14039613a34737bb84ada147fc
A.Harshitha <119464978+Harshitha-reddy88@users.noreply.github.com>
2023-04-20 15:49:43 +0530

    multiplication of array elements

---
+++ week_01/day_1/arrmul.java
@@ -0,0 +1,13 @@
+public class Main
+{
+   public static void main(String[] args)
+   {
+      int[] arr={2,2,2,2};
+      int mul=1;
+      for(int i=0;i<arr.length;i++)
+      {
+         mul=mul*arr[i];
+      }
+      System.out.println(mul);
+    }
+ }
